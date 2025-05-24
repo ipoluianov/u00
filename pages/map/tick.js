5f39b22029001bc86e57ad4dc9299633903be8d7
ip <ivan.poluianov@gmail.com>
2025-05-24 10:27:57 +0200

    Update tick.js

--- pages/map/tick.js
+++ pages/map/tick.js
@@ -2,6 +2,25 @@ APP.draw = function (elId) {
     drawAppTextWithHeader(APP);
 }
 
+APP.wakeLocked = false;
+
+APP.wakeLock = async function () {
+    if (APP.wakeLocked) {
+        console.log('Wake Lock is already active');
+        return;
+    }
+    try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        wakeLock.addEventListener('release', () => {
+            console.log('Screen Wake Lock was released');
+        });
+        console.log('Screen Wake Lock is active');
+        APP.wakeLocked = true;
+    } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+    }
+}
+
 APP.tick = function () {
     /*const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
@@ -13,6 +32,8 @@ APP.tick = function () {
 
     if ("wakeLock" in navigator) {
         comment = "Wake lock is supported by this browser.";
+        APP.wakeLock();
+
     } else {
         comment = "Wake lock is NOT supported by this browser.";
     }
