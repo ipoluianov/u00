f6d880764ae522c0a07b9b9331dbfb78c98fac3f
ip <ivan.poluianov@gmail.com>
2025-05-24 10:24:06 +0200

    map

--- pages/map/tick.js
+++ pages/map/tick.js
@@ -1,18 +1,30 @@
-APP.draw = function(elId) {
+APP.draw = function (elId) {
     drawAppTextWithHeader(APP);
 }
 
-APP.tick = function() {
-    const now = new Date();
+APP.tick = function () {
+    /*const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    let timeString = `${hours}:${minutes}:${seconds}`;
+    let timeString = `${hours}:${minutes}:${seconds}`;*/
 
-    const offsetMinutes = new Date().getTimezoneOffset();
-    let offsetHours = -offsetMinutes / 60;
-    
-    let utcOffsetStr = `UTC${offsetHours >= 0 ? '+' : ''}${offsetHours}`;
+    var comment = "";
 
-    APP.data = {Header: "Temperature", Value2: APP.data_value, Comment: ""};
+    if ("wakeLock" in navigator) {
+        comment = "Wake lock is supported by this browser.";
+    } else {
+        comment = "Wake lock is NOT supported by this browser.";
+    }
+
+    // parse json data
+    if (APP.data_value === undefined) {
+        APP.data = { Header: "Temperature", Value2: "NO DATA", Comment: comment };
+        return;
+    }
+
+    var obj = JSON.parse(APP.data_value)
+
+
+    APP.data = { Header: obj.d, Value2: obj.v, Comment: comment };
 }
